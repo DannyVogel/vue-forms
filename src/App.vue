2ace31ba2c0980ae75b9a6dc075bcca5d7d856d7
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-25 10:14:47 +0200

    initial project setup

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <SignUpForm />
+  </div>
+</template>
+
+<script>
+import SignUpForm from './components/SignUpForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUpForm
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <form>
+        <label>Email:</label>
+        <input type="email" name="email" placeholder="Enter your email address" v-model="email"/>
+        <label>Password:</label>
+        <input type="password" name="password" placeholder="Enter your password" v-model="password"/>
+    </form>
+    <p>Email: {{email}}</p>
+    <p>Password: {{password}}</p>
+</template>
+
+<script>
+export default {
+    name: 'SignUpForm',
+    components: {
+
+    },
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    }
+
+}
+
+</script>
+
+<style>
+    form {
+        max-width: 420px;
+        margin: 30px auto;
+        padding: 40px;
+        background: #fff;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    label {
+        display: inline-block;
+        margin-bottom: 25px 0 15px;
+        font-size: 0.6em;
+        color: #aaa;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        font-weight: bold;
+        text-align: left;
+    }
+    input {
+        display: block;
+        box-sizing: border-box;
+        width: 100%;
+        padding: 10px 6px;
+        color: #555;
+        border: none;
+        border-bottom: 1px solid #ddd;
+    }
+</style>
